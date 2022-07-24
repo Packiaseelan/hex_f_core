950d6bfb547e4c55d818d91b2691f888a9818dcd
Packiaseelan S <packiaseelan14@gmail.com>
2022-07-24 13:40:43 +0530

    hex flutter core lib init commit

---
+++ scripts/clean_all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+(cd core/app-mobile-core/src && flutter clean)
+(cd core/app-mobile-core/src && rm pubspec.lock)
+(cd core/app-mobile-core/src/example && flutter clean)
+(cd core/app-mobile-core/src/example && rm pubspec.lock)
+(cd widgets/app-mobile-widgets/src && flutter clean)
+(cd widgets/app-mobile-widgets/src && rm pubspec.lock)
---
+++ scripts/pub_get_all.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+(cd core/app-mobile-core/src && pwd && flutter pub get)
+(cd widgets/app-mobile-widgets/src && pwd && flutter pub get)
