b915b5153a78a55a3a42f3dff6ed8652223cab9d
Packiaseelan S <packiaseelan14@gmail.com>
2022-07-25 23:46:09 +0530

    network manager init

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 (cd core/app-mobile-core/src && flutter clean)
 (cd core/app-mobile-core/src && rm pubspec.lock)
+
 (cd core/app-mobile-core/src/example && flutter clean)
 (cd core/app-mobile-core/src/example && rm pubspec.lock)
+
 (cd widgets/app-mobile-widgets/src && flutter clean)
 (cd widgets/app-mobile-widgets/src && rm pubspec.lock)
+
+(cd network_manager/app-mobile-network_manager/src && flutter clean)
+(cd network_manager/app-mobile-network_manager/src && rm pubspec.lock)
--- scripts/pub_get_all.sh
+++ scripts/pub_get_all.sh
@@ -2,3 +2,4 @@
 (cd core/app-mobile-core/src && pwd && flutter pub get)
 (cd widgets/app-mobile-widgets/src && pwd && flutter pub get)
 (cd example_mobile_app && pwd && flutter pub get)
+(cd network_manager/app-mobile-network_manager/src && flutter pub get)
